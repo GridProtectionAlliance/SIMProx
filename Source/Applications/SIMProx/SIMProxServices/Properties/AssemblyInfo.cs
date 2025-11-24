641d95f4ab16bd017ab22d527689af2630bf467f
gsfbuildbot <support@gridprotectionalliance.org>
2025-11-24 03:24:54 -0500

    SIMProx: Version change for build v1.0.396.0-master.

--- Source/Applications/SIMProx/SIMProxServices/Properties/AssemblyInfo.cs
+++ Source/Applications/SIMProx/SIMProxServices/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.0.395.0")]
+[assembly: AssemblyVersion("1.0.396.0")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
